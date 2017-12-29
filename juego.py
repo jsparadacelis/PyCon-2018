40bc92eab3b889bd23312aaf45291a53ca41807e
Sebastián Parada <parada.celis@gmail.com>
2017-12-29 16:49:37 -0500

    Contando los vecinos

--- juego.py
+++ juego.py
@@ -15,37 +15,39 @@ def imprimirTabla(tabla):
 		print
 
 def revisarVecinos(tabla, pos): ##Revision de los vecinos de cada posicion
-	print str(pos)
-
+	
+	vecinos = 0
 	if pos == (0,0):
-			print "soy cero cesro"
-			for k in tabla[0:pos[0]+2]:
-				for j in k[0:pos[1]+2]:
-					print j,
-				print
-			print "******************"
+		for k in tabla[0:pos[0]+2]:
+			for j in k[0:pos[1]+2]:
+				if j == 1:
+					vecinos+=1
 
 	elif pos[0] == 0:
 		for k in tabla[0:pos[0]+2]:
 			for j in k[pos[1]-1:pos[1]+2]:
-				print j,
-			print
-		print "******************"
-		
+				if j == 1:
+					vecinos+=1
+
 	elif pos[1] == 0:
-			for k in tabla[pos[0]-1:pos[0]+2]:
-				for j in k[0:pos[1]+2]:
-					print j,
-				print
-			print "******************"
+		for k in tabla[pos[0]-1:pos[0]+2]:
+			for j in k[0:pos[1]+2]:
+				if j == 1:
+					vecinos+=1
 	
 	else:
 		for k in tabla[pos[0]-1:pos[0]+2]:
 			for j in k[pos[1]-1:pos[1]+2]:
-				print j,
-			print
-		print "******************"
+				if j == 1:
+					vecinos+=1
 
+
+	if tabla[int(pos[0])][int(pos[1])] == 1:
+		vecinos-=1
+
+	
+	return vecinos
+ 
 c = 0
 
 
@@ -54,7 +56,7 @@ vecinosVivos = 0
 for i in range(5):
 	for j in range(5):
 			print str(i)+"-"+str(j)
-			revisarVecinos(a,(i,j))
+			print str(revisarVecinos(a,(i,j)))
 		
 c = c+1
 imprimirTabla(a)
