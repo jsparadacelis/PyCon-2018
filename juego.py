1f20bc6c26f93d4a9e5b6b129d3755289fe7ce9d
Sebastián Parada <parada.celis@gmail.com>
2017-12-29 14:14:01 -0500

    impresión de los vecinos de cada posición

--- juego.py
+++ juego.py
@@ -1,8 +1,12 @@
 a = [[0,0,0,0,0],
-	 [0,0,1,0,0],
-	 [0,1,1,1,0],
-	 [0,0,1,0,0],
-	 [0,0,0,0,0]]
+ 	 [0,0,1,0,0],
+ 	 [0,1,1,1,0],
+ 	 [0,0,1,0,0],
+ 	 [0,0,0,0,0]]
+
+#a = [[0,1,0],
+#	 [1,1,1],
+#	 [0,1,0]]
 
 def imprimirTabla(tabla):
 	for k in tabla:
@@ -10,22 +14,53 @@ def imprimirTabla(tabla):
 			print j,
 		print
 
-def revisarVecinos(tabla, pos):
-	print str(pos[0])
+def revisarVecinos(tabla, pos): ##Revision de los vecinos de cada posicion
+	print str(pos)
+
+	if pos == (0,0):
+			print "soy cero cesro"
+			for k in tabla[0:pos[0]+2]:
+				for j in k[0:pos[1]+2]:
+					print j,
+				print
+			print "******************"
+
+	elif pos[0] == 0:
+		for k in tabla[0:pos[0]+2]:
+			for j in k[pos[1]-1:pos[1]+2]:
+				print j,
+			print
+		print "******************"
+		
+	elif pos[1] == 0:
+			for k in tabla[pos[0]-1:pos[0]+2]:
+				for j in k[0:pos[1]+2]:
+					print j,
+				print
+			print "******************"
+	
+	else:
+		for k in tabla[pos[0]-1:pos[0]+2]:
+			for j in k[pos[1]-1:pos[1]+2]:
+				print j,
+			print
+		print "******************"
 
 c = 0
-while c < 5:
-	imprimirTabla(a)
-	print "\n"
-	vecinosVivos = 0
-	for i in range(5):
-		for j in range(5):
+
+
+print "\n"
+vecinosVivos = 0
+for i in range(5):
+	for j in range(5):
+			print str(i)+"-"+str(j)
 			revisarVecinos(a,(i,j))
-			
-	c = c+1
-	imprimirTabla(a)
-	print "\n"
-	
+		
+c = c+1
+imprimirTabla(a)
+print "--------------------------"
+print "\n"
+
 
 
 
