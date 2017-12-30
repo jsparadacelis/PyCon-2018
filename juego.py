5322c3f77459689d528f0dbebc0a0c7ed496b366
Sebastián Parada <parada.celis@gmail.com>
2017-12-29 20:20:31 -0500

    Listas las transformaciones

--- juego.py
+++ juego.py
@@ -1,8 +1,20 @@
-a = [[0,0,0,0,0],
- 	 [0,0,1,0,0],
- 	 [0,1,1,1,0],
- 	 [0,0,1,0,0],
- 	 [0,0,0,0,0]]
+# a = [[0,0,0,0,0,0],
+#  	 [0,0,1,1,0,0],
+#  	 [0,1,0,0,1,0],
+#  	 [0,0,1,0,1,0],
+#  	 [0,0,0,1,0,0],
+#  	 [0,0,0,0,0,0]]
+
+a = [[0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,1,0,0,0,0],
+ 	 [0,0,0,0,1,0,0,0,0,0],
+ 	 [0,0,0,0,1,1,1,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0]]
 
 #a = [[0,1,0],
 #	 [1,1,1],
@@ -42,24 +54,80 @@ def revisarVecinos(tabla, pos): ##Revision de los vecinos de cada posicion
 					vecinos+=1
 
 
-	if tabla[int(pos[0])][int(pos[1])] == 1:
-		vecinos-=1
+
+	if(tabla[pos[0]][pos[1]] == 1):
+		vecinos = vecinos -1
 
 	
 	return vecinos
  
-c = 0
 
+#Una celula muerta con exactamente 3 celulas vecinas vivas "nace" (es decir, al turno siguiente estara viva).
+#Una celula viva con 2 o 3 celulas vecinas vivas sigue viva, en otro caso muere o permanece muerta (por "soledad" o "superpoblacion").
 
 print "\n"
 vecinosVivos = 0
-for i in range(5):
-	for j in range(5):
-			print str(i)+"-"+str(j)
-			print str(revisarVecinos(a,(i,j)))
-		
-c = c+1
 imprimirTabla(a)
+print "--------------------------"
+print "\n"
+
+t = [[0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0]]
+
+for i in range(10):
+	for j in range(10):
+		
+		vecinosVivos = revisarVecinos(a,(i,j))
+		
+		if a[i][j] == 0 and vecinosVivos == 3:
+			t[i][j] = 1
+		elif a[i][j] == 1 and vecinosVivos < 2:
+			t[i][j] = 0
+		elif a[i][j] == 1 and vecinosVivos > 3:
+			t[i][j] = 0
+		elif a[i][j] == 1 and (vecinosVivos == 2 or vecinosVivos == 3):
+			t[i][j] = 1
+
+		
+
+
+imprimirTabla(t)
+print "--------------------------"
+print "\n"
+
+r = [[0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0],
+ 	 [0,0,0,0,0,0,0,0,0,0]]
+
+for i in range(10):
+	for j in range(10):
+		vecinosVivos = revisarVecinos(t,(i,j))
+		if t[i][j] == 0 and vecinosVivos == 3:
+			r[i][j] = 1
+		elif t[i][j] == 1 and vecinosVivos < 2:
+			r[i][j] = 0
+		elif t[i][j] == 1 and vecinosVivos > 3:
+			r[i][j] = 0
+		elif t[i][j] == 1 and (vecinosVivos == 2 or vecinosVivos == 3):
+			r[i][j] = 1
+
+
+imprimirTabla(r)
 print "--------------------------"
 print "\n"
 
