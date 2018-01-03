9f7b592ac2ee76b4a128792b109da57903c89fe4
Sebastián Parada <parada.celis@gmail.com>
2018-01-03 15:28:55 -0500

    Impresión de listas y matrices

--- pruebas.py
+++ pruebas.py
@@ -1,11 +1,20 @@
-a = [0,1,2]
+lista = [0,1,2]
+matriz = [[0,1,2],[0,2,3],[2,3,4]]
+ 
 
 def imprimirLista(a):
-
 	if len(a) == 1:
-		print str(a[0])
+		return str(a[0])
 	else:
-		print str(a[0])
-		return imprimirLista(a[1:])
+		return str(a[0]) + str(imprimirLista(a[1:]))
 
-imprimirLista(a)
+
+
+def imprimirMatriz(a):
+	if len(a) == 1:
+		return str(imprimirLista(a[0]))
+	else:
+		return str(imprimirLista(a[0]))+"\n"+str(imprimirMatriz(a[1:]))
+
+
+print imprimirMatriz(matriz)	
