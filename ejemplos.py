91b2235497451366da7df207606d4284a77079ec
Sebastián Parada <parada.celis@gmail.com>
2018-01-18 21:04:21 -0500

    Añadiendo ejemplos para pycon

--- ejemplos.py
+++ ejemplos.py
@@ -1,28 +1,6 @@
 from functools import reduce
 from operator import mul
 import timeit
-
-lista = [0,1,2]
-matriz = [[0,1,2],[0,2,3],[2,3,4]]
- 
-
-def imprimirLista(a):
-	if len(a) == 1:
-		return str(a[0])
-	else:
-		return str(a[0]) + str(imprimirLista(a[1:]))
-
-
-
-def imprimirMatriz(a):
-	if len(a) == 1:
-		return str(imprimirLista(a[0]))
-	else:
-		return str(imprimirLista(a[0]))+"\n"+str(imprimirMatriz(a[1:]))
-
-
-print imprimirMatriz(matriz)	
-
 naturales = range(1,11)
 
 
